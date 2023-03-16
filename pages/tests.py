1140ce97b48f3d1105c564edaf193ccd5701dda9
Mishael Ratemo <ratemomishael@gmail.com>
2023-03-16 17:51:27 +0300

    using SetUP method to avoid repetiton(DRY)

--- pages/tests.py
+++ pages/tests.py
@@ -2,24 +2,28 @@ from django.test import SimpleTestCase
 from django.urls import reverse
 
 class HomepageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+    def setUp(self): # new
+        url = reverse("home")
+        self.response = self.client.get(url)
 
-    def test_homepage_url_name(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
+    def test_url_exists_at_correct_location(self):
+        # response = self.client.get("/")
+        self.assertEqual(self.response.status_code, 200)
+
+    # def test_homepage_url_name(self):
+    #     # response = self.client.get(reverse("home"))
+    #     self.assertEqual(self.response.status_code, 200)
 
     #testing templatess - home.html
     def test_homepage_template(self): # new
-        response = self.client.get("/")
-        self.assertTemplateUsed(response, "home.html")
+        # response = self.client.get("/")
+        self.assertTemplateUsed(self.response, "home.html")
 
     # testing correct HTML
     def test_homepage_does_not_contain_incorrect_html(self):
-        response = self.client.get('/')
-        self.assertNotContains(response, "Hello, Am not supposed to be here, Something is wrong!!")
+        # response = self.client.get('/')
+        self.assertNotContains(self.response, "Hello, Am not supposed to be here, Something is wrong!!")
 
     def test_homepage_contains_correct_html(self):
-        resp =self.client.get('/')
-        self.assertContains(resp, "home page")
+        # resp =self.client.get('/')
+        self.assertContains(self.response, "home page")
