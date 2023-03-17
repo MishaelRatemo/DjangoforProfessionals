5418ae8e104e0005102e31c2bdf1eeef73319d22
Mishael Ratemo <ratemomishael@gmail.com>
2023-03-17 10:52:43 +0300

    added spaces before and after build in the los file

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "\n******************* $(date): Starting script **************\n\n" >> /home/mishael/coding/python/DJANGODevOps/bookstore/build.log 2>&1
+echo "******************* $(date): Starting script **************" >> /home/mishael/coding/python/DJANGODevOps/bookstore/build.log 2>&1
+
+echo 
 
 # Navigate to the directory where your Docker Compose file is located
 cd /home/mishael/coding/python/DJANGODevOps/bookstore/
@@ -14,4 +16,6 @@ docker compose up -d --build
 # Monitor your containers
 docker compose ps
 
-echo "\n\n##############  $(date): Script completed Succcessfully  #############\n\n" >> /home/mishael/coding/python/DJANGODevOps/bookstore/build.log 2>&1
+echo "##############  $(date): Script completed Succcessfully  #############" >> /home/mishael/coding/python/DJANGODevOps/bookstore/build.log 2>&1
+
+echo
