088922ffc7a5d0041cd51a0feb2c3a43e0b3d90e
Mishael Mosoti Ratemo <ratemomishael@gmail.com>
2024-02-08 16:48:33 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse, resolve 
-
+  
 
 from .forms import CustomUserCreationForm
 # from .views import SignupPageView
